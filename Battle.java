b64739581a1347a9fed8f003fc85fd476ea09ef9
Ashar <126603854+RIP2483@users.noreply.github.com>
2024-07-24 03:19:14 +0800

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -1,25 +1,57 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Battle {
-    public static void main(String[] args) {
-        Pokemon pikachu = new Pikachu();
-        Pokemon charizard = new Charizard();
+    private Pokemon playerPokemon;
+    private Pokemon opponentPokemon;
+    private Scanner scanner;
 
-        System.out.println("Battle Start!");
-        System.out.println(pikachu.getName() + " vs " + charizard.getName());
+    public Battle(Pokemon playerPokemon, Pokemon opponentPokemon) {
+        this.playerPokemon = playerPokemon;
+        this.opponentPokemon = opponentPokemon;
+        this.scanner = new Scanner(System.in);
+    }
 
-        while (pikachu.getHp() > 0 && charizard.getHp() > 0) {
-            pikachu.attack(charizard);
-            if (charizard.getHp() <= 0) {
-                System.out.println(charizard.getName() + " is defeated!");
-                break;
+    public void start() {
+        System.out.println("Battle start!");
+        System.out.println(playerPokemon.getName() + " vs " + opponentPokemon.getName());
+
+        while (!playerPokemon.isFainted() && !opponentPokemon.isFainted()) {
+            playerTurn();
+            if (opponentPokemon.isFainted()) {
+                System.out.println(opponentPokemon.getName() + " fainted! " + playerPokemon.getName() + " wins!");
+                return;
             }
 
-            charizard.attack(pikachu);
-            if (pikachu.getHp() <= 0) {
-                System.out.println(pikachu.getName() + " is defeated!");
-                break;
+            opponentTurn();
+            if (playerPokemon.isFainted()) {
+                System.out.println(playerPokemon.getName() + " fainted! " + opponentPokemon.getName() + " wins!");
+                return;
             }
         }
+    }
 
-        System.out.println("Battle End!");
+    private void playerTurn() {
+        System.out.println("Choose a move:");
+        for (int i = 0; i < playerPokemon.getMoves().size(); i++) {
+            System.out.println((i + 1) + ". " + playerPokemon.getMoves().get(i).getName());
+        }
+        int choice = scanner.nextInt();
+        Move move = playerPokemon.getMoves().get(choice - 1);
+        useMove(playerPokemon, opponentPokemon, move);
+    }
+
+    private void opponentTurn() {
+        Random random = new Random();
+        int choice = random.nextInt(opponentPokemon.getMoves().size());
+        Move move = opponentPokemon.getMoves().get(choice);
+        useMove(opponentPokemon, playerPokemon, move);
+    }
+
+    private void useMove(Pokemon attacker, Pokemon defender, Move move) {
+        int damage = (attacker.getAttack() * move.getPower()) / defender.getDefense();
+        System.out.println(attacker.getName() + " used " + move.getName() + "!");
+        defender.takeDamage(damage);
+        System.out.println(defender.getName() + " took " + damage + " damage!");
     }
 }
