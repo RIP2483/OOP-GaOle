aaab20ff40827bcee7fa7af008c0c72baca423ee
Ashar <126603854+RIP2483@users.noreply.github.com>
2024-07-25 20:15:55 +0800

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,10 +1,12 @@
 public class Move {
     private String name;
     private int power;
+    private String type;
 
-    public Move(String name, int power) {
+    public Move(String name, int power, String type) {
         this.name = name;
         this.power = power;
+        this.type = type;
     }
 
     public String getName() {
@@ -13,5 +15,31 @@ public class Move {
 
     public int getPower() {
         return power;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+
+    public String toString() {
+        return "Move{" +
+                "name='" + name + '\'' +
+                ", power=" + power +
+                ", type='" + type + '\'' +
+                '}';
     }
 }
