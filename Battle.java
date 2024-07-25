52e4b9127e9ee5f7a5654d42ec95eadc0951d715
Ashar <126603854+RIP2483@users.noreply.github.com>
2024-07-25 22:46:29 +0800

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -16,26 +16,26 @@ public class Battle {
     }
 
     public void start() {
-        System.out.println("Battle start!");
-        System.out.println(playerPokemon.getName() + " vs " + opponentPokemon.getName());
+        TextUtils.printWithDelay("Battle start!");
+        TextUtils.printWithDelay(playerPokemon.getName() + " vs " + opponentPokemon.getName());
 
         while (!playerPokemon.isFainted() && !opponentPokemon.isFainted()) {
             playerTurn();
             if (opponentPokemon.isFainted()) {
-                System.out.println(opponentPokemon.getName() + " fainted! " + playerPokemon.getName() + " wins!");
+                TextUtils.printWithDelay(opponentPokemon.getName() + " fainted! " + playerPokemon.getName() + " wins!");
                 return;
             }
 
             opponentTurn();
             if (playerPokemon.isFainted()) {
-                System.out.println(playerPokemon.getName() + " fainted! " + opponentPokemon.getName() + " wins!");
+                TextUtils.printWithDelay(playerPokemon.getName() + " fainted! " + opponentPokemon.getName() + " wins!");
                 return;
             }
         }
     }
 
     private void playerTurn() {
-        System.out.println("Choose a move:");
+        TextUtils.printWithDelay("Choose a move:");
         for (int i = 0; i < playerPokemon.getMoves().size(); i++) {
             System.out.println((i + 1) + ". " + playerPokemon.getMoves().get(i).getName());
         }
@@ -54,10 +54,10 @@ public class Battle {
     private void useMove(Pokemon attacker, Pokemon defender, Move move) {
         double effectiveness = TypeEffectiveness.getEffectiveness(move.getType(), defender.getType());
         int damage = calculateDamage(move.getPower(), attacker.getAttack(), defender.getDefense(), effectiveness);
-        System.out.println(attacker.getName() + " used " + move.getName() + "!");
-        System.out.println("It's " + getEffectivenessString(effectiveness) + " effective!");
+        TextUtils.printWithDelay(attacker.getName() + " used " + move.getName() + "!");
+        TextUtils.printWithDelay("It's " + getEffectivenessString(effectiveness) + " effective!");
         defender.takeDamage(damage);
-        System.out.println(defender.getName() + " took " + damage + " damage!");
+        TextUtils.printWithDelay(defender.getName() + " took " + damage + " damage!");
     }
 
     private int calculateDamage(int power, int attack, int defense, double effectiveness) {
