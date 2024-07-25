d65362281c2ac3e62cd7d433774e09eb2bf7e890
Ashar <126603854+RIP2483@users.noreply.github.com>
2024-07-25 20:16:39 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Player {
+    Scanner scanner = new Scanner(System.in);
+
     private String name;
     private List<Pokemon> pokemonList;
 
@@ -16,5 +19,35 @@ public class Player {
 
     public List<Pokemon> getPokemonList() {
         return pokemonList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void ViewCapturedPokemon() {
+        if (pokemonList.isEmpty()) {
+            System.out.println("You have not captured any Pokemon yet.");
+        }
+        else {
+            System.out.println("Captured Pokemon: ");
+            for (Pokemon pokemon : pokemonList) {
+                System.out.println(pokemon.getName());
+            }
+        }
+    }
+
+    public int SelectPokemon() {
+        System.out.println("Choose your Pokemon: ");
+        for (int i = 0; i < pokemonList.size(); i++) {
+            System.out.println(i + 1 + ". " + pokemonList.get(i).getName());
+        }
+        int choice = scanner.nextInt();
+        int index = choice - 1;
+        return index;
     }
 }
