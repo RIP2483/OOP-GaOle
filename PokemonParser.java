50c5bf9dadd0a69314f4b0fd89f4537f0a1c26ff
Ashar <126603854+RIP2483@users.noreply.github.com>
2024-07-25 20:17:30 +0800

    Update PokemonParser.java

--- PokemonParser.java
+++ PokemonParser.java
@@ -7,13 +7,16 @@ public class PokemonParser {
         List<Pokemon> pokemonList = new ArrayList<>();
 
         for (String[] row : data) {
+            int id = Integer.parseInt(row[0]);
             String name = row[1];
+            String type = row[2];
             int hp = Integer.parseInt(row[3]);
             int attack = Integer.parseInt(row[4]);
             int defense = Integer.parseInt(row[5]);
             int speed = Integer.parseInt(row[6]);
+            int grade = Integer.parseInt(row[7]);
 
-            pokemonList.add(new Pokemon(name, hp, attack, defense, speed, new ArrayList<>()));
+            pokemonList.add(new Pokemon(id, name, type, hp, attack, defense, speed, grade,  new ArrayList<>()));
         }
         return pokemonList;
     }
