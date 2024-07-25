5bcc003d552eb44a72f558dc542c49fae5f51099
Ashar <126603854+RIP2483@users.noreply.github.com>
2024-07-25 20:17:15 +0800

    Update PokemonGame.java

--- PokemonGame.java
+++ PokemonGame.java
@@ -5,9 +5,9 @@ public class PokemonGame {
         List<Pokemon> wildPokemonList = PokemonParser.parsePokemonData("C:\\Users\\ashar\\Desktop\\Uni work\\Sem 2\\OOP\\final assignment\\final project\\src\\pokemon.csv");
         List<Move> moveList = MoveParser.parseMoveData("C:\\Users\\ashar\\Desktop\\Uni work\\Sem 2\\OOP\\final assignment\\final project\\src\\moves.csv");
 
-        Pokemon pikachu = new Pokemon("Pikachu", 100, 55, 40, 90, List.of(new Move("Thunder Shock", 40), new Move("Quick Attack", 30)));
+        // Pokemon pikachu = new Pokemon("Pikachu", 100, 55, 40, 90, List.of(new Move("Thunder Shock", 40), new Move("Quick Attack", 30)));
         Player player = new Player("Ash");
-        player.addPokemon(pikachu);
+        //player.addPokemon(pikachu);
 
         Game game = new Game(player, wildPokemonList, moveList);
         game.start();
